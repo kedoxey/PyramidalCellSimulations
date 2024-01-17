dc30c9648a01bfb41cfd3c1e513e7af720b20353
Kate Doxey <kedoxey@gmail.com>
2024-01-16 18:10:43 -0700

    updated script to import neuroml version

--- model_helpers.py
+++ model_helpers.py
@@ -1,8 +1,14 @@
 import requests
 import json
 import os
+import pyneuroml as pynml
+import neuroml as nml
+import neuroml.writers as writers
+from neuroml.utils import component_factory
 from zipfile import ZipFile
 from urllib.request import urlopen
+
+from neuromllite import Network, Cell, Population, InputSource, Input, NetworkGenerator
 
 cwd = os.getcwd()
 
@@ -39,3 +45,31 @@ def download_from_nmldb(model_id, version):
         print(f'Model {model_id} already downloaded.')
 
     
+def generate_network(nml_dir, cell_name, pop_label, pop_size=1, force=False, **input_args):
+    
+    cell_nml_path = os.path.join(nml_dir, f'{cell_name}.cell.nml')
+    net_nml_path = os.path.join(nml_dir, f'{cell_name}_Net.net.nml')
+
+    if force or not os.path.exists(net_nml_path):
+
+        net = Network(id=f'{cell_name}_Net')
+        net.notes = 'Hay et al. 2011 model of Neocortex Layer 5 Pyramidal Cell for import into NetPyNE'
+        net.parameters = {'input_amp': input_args['input_amp']}
+
+        cell = Cell(id=cell_name, neuroml2_source_file=cell_nml_path)
+        net.cells.append(cell)
+
+        pop = Population(id=pop_label,
+                         size=pop_size,
+                         component=cell.id)
+        net.populations.append(pop)
+
+        NetworkGenerator.generate_neuroml2_from_network(net,
+                                                        nml_file_name=net_nml_path,
+                                                        format='xml')
+        
+        print(f'Network for {cell_name} successfully generated!')
+    else:
+        print(f'Network for {cell_name} already generated!')
+
+    return net_nml_path
--- simulate_netpyne.py
+++ simulate_netpyne.py
@@ -4,21 +4,28 @@ import json
 import model_helpers as mh
 from neuron import h, load_mechanisms
 from netpyne import specs, sim
+import time
 
+start_t = time.time()
 
 ### Parameters for simulation ###
-test_name = 'nmldb_trace'
+run_NML = True
+test_name = 'test_nml'
 hoc_fname = 'L5PC'
 vinit = -80
 
-input_amps = [0.35477, 0.44346, 0.53215, 1.0643]
-amp_idx = 0  # if -1 then no current injection
+### Input parameters ###
+input_amps = [0, 0.35477, 0.44346, 0.53215, 1.0643]
+amp_idx = 4  
 input_amp = input_amps[amp_idx]
+in_delay = 700
+in_dur = 2000
+
 test_label = f'input_{amp_idx}-{test_name}'
 
 ### Model information ###
 code_version = 'Hay'
-model_version = 'NEURON'
+model_version = 'NeuroML' if run_NML else 'NEURON'
 
 nmldb_id = 'NMLCL000073'  # 'NMLCL000073' (Hay et al. 2011)
 model_name = f'{nmldb_id}-{model_version}'
@@ -27,6 +34,7 @@ cell_model = 'Hay2011'  # 'Hay2011'
 cell_type = 'PYR'
 cell_name = 'L5PC'  # 'L5PC'
 cell_label = cell_name+'_hoc'
+pop_label = cell_name+'_Pop'
 
 ### Define paths ###
 cwd = os.getcwd()
@@ -44,48 +52,17 @@ output_dir = os.path.join(model_dir,'output')
 if not os.path.exists(output_dir):
     os.mkdir(output_dir)
 
+### Generate network if running NeuroML ###
+if run_NML:
+    net_nml_file = mh.generate_network(model_dir, cell_name, pop_label, 
+                                       force=True, 
+                                       input_amp=input_amp, 
+                                       start=in_delay, 
+                                       stop=in_delay+in_dur)
+
 ### Compile mechs ###
 mh.compile_mechs(cwd,hocs_dir,mod_dir)
 load_mechanisms(model_dir)
-
-### Network Parameters ###
-netParams = specs.NetParams()
-
-### Import cell into NetPyNE ###
-importedCellParams = netParams.importCellParams(label=cell_label,
-                                                conds={'cellType': cell_type, 'cellModel': cell_model},
-                                                fileName=hoc_file,
-                                                cellName=cell_name
-                                                )
-
-for sec in importedCellParams['secs']:
-    importedCellParams[sec]['vinit'] = vinit
-
-### Create population ###
-pop_label = cell_label+'_pop'
-netParams.popParams[pop_label] = {'cellType': cell_type, 
-                                  'cellModel': cell_model,
-                                  'numCells': 1}
-
-### Add input ###
-if amp_idx > -1:
-    netParams.stimSourceParams['Input_IC'] = {
-        'type': 'IClamp',
-        'del': 700,
-        'dur': 2000,
-        'amp': input_amp  
-    }
-
-    netParams.stimTargetParams['Input_IC->Soma'] = {
-        'source': 'Input_IC',
-        'sec': 'soma_0',
-        'loc': 0.5,
-        'conds': {'pop': pop_label}
-    }
-
-    print(f'current clamp added at {input_amp} pA')
-else:
-    print('no input')
 
 ### Simulation configuration ###
 cfg = specs.SimConfig()					            # object of class SimConfig to store simulation configuration
@@ -100,8 +77,54 @@ cfg.analysis['plotTraces'] = {'include': ['all'], 'saveFig': True} # Plot record
 cfg.hParams['celsius'] = 34.0 
 cfg.hParams['v_init'] = vinit
 
+### Import cell ###
+if run_NML:
+    gid, netParams = sim.importNeuroML2(net_nml_file, simConfig=cfg, simulate=False, analyze=False, return_net_params_also=True)
+    # sim.importNeuroML2SimulateAnalyze(net_nml_file, simConfig=cfg)
+else:
+    netParams = specs.NetParams()
+
+    importedCellParams = netParams.importCellParams(label=cell_label,
+                                                    conds={'cellType': cell_type, 'cellModel': cell_model},
+                                                    fileName=hoc_file,
+                                                    cellName=cell_name
+                                                    )
+
+    for sec in importedCellParams['secs']:
+        importedCellParams[sec]['vinit'] = vinit
+
+    ### Create population ###
+    netParams.popParams[pop_label] = {'cellType': cell_type, 
+                                    'cellModel': cell_model,
+                                    'numCells': 1}
+
+### Add input ###
+netParams.stimSourceParams['Input_IC'] = {
+    'type': 'IClamp',
+    'del': in_delay,
+    'dur': in_dur,
+    'amp': input_amp  
+}
+
+netParams.stimTargetParams['Input_IC->Soma'] = {
+    'source': 'Input_IC',
+    'sec': 'soma_0',
+    'loc': 0.5,
+    'conds': {'pop': pop_label}
+}
+
 ### Run simulation ###
-(_, _, _, _, simData) = sim.createSimulateAnalyze(netParams=netParams, simConfig=cfg, output=True)
+sim.createSimulateAnalyze(netParams=netParams, simConfig=cfg, output=False)
+
+### Simulation time ###
+end_t = time.time()
+final_t = end_t - start_t
+m, s = divmod(final_t, 60)
+time_path = os.path.join(output_dir,f'{model_version}_{test_label}_sim-time.txt')
+
+with open(time_path, "w") as text_file:
+    text_file.write(f'Simulation time = {m} m {s} s')
+print(f'Simulation time = {m} m {s} s')
 
 ### Plot morphology ###
-sim.analysis.plotShape(showSyns=True, dist=0.8, saveFig=True, axisLabels=True)
+# sim.analysis.plotShape(showSyns=True, dist=0.8, saveFig=True, axisLabels=True)
