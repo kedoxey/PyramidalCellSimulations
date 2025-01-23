fa3c904891f076c598f5001dd6a686b2ed3d3088
Kate Doxey <kedoxey@gmail.com>
2025-01-23 13:36:42 -0700

    parameters for running simulation with 30 probes with 64 channels

--- batch_simulate_netpyne.py
+++ batch_simulate_netpyne.py
@@ -15,8 +15,8 @@ stim_dur = sim_dur
 
 param_sets = {#'active': {'log_firing_rate': True}
               'probes': {'use_probes': [True],
-                         'num_probes': [2],
-                         'total_channels': [12]}}
+                         'num_probes': [30],
+                         'total_channels': [64]}}
               # 'pas_soma': {'channel_toggles': {'Na': 0, 'Ca': 0, 'K': 0},
               #                'channel_secs': 'soma'},
               # 'pas_all': {'channel_toggles': {'Na': 0, 'Ca': 0, 'K': 0},
