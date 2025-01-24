28007de04814c23ea2a31c82ca280f297623e5f3
Kate Doxey <kedoxey@gmail.com>
2025-01-23 21:26:54 -0700

    update to notebook to plot EAP kernel for n furthest and closest probes

--- batch_run_simulation.py
+++ batch_run_simulation.py
@@ -14,8 +14,8 @@ stim_delay = 0 if 'FR' in sim_name else 400
 stim_dur = sim_dur
 
 param_sets = {#'active': {'log_firing_rate': True}
-              'TEST': {'use_probes': [True],
-                         'num_probes': [30],
+              'probes': {'use_probes': [True],
+                         'num_probes': [100],
                          'total_channels': [64]}}  # total_channels must be greater than 1
               # 'pas_soma': {'channel_toggles': {'Na': 0, 'Ca': 0, 'K': 0},
               #                'channel_secs': 'soma'},
