c8b5b0cc0ef1acc92c08a5b5a185d46d62ac9405
Kate Doxey <kedoxey@gmail.com>
2025-03-20 14:34:28 -0700

    waveform feature extraction of model EAP kernels

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ['setup', 'config', 'plotting', 'process', 'wrangle']
+__all__ = ['setup', 'config', 'plotting', 'process', 'waveform_metrics', 'wrangle']
 
 from . import setup 
 from . import config
 from . import plotting
 from . import process
+from . import waveform_metrics
 from . import wrangle
